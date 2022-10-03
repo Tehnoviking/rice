95610560fe125d22c5e5f1807dc2b381c5903fe2
Tehnoviking <alexmaria1506@gmail.com>
2022-10-03 20:38:12 +0300

    homework

---
+++ home work 1.py
@@ -0,0 +1,45 @@
+#zadacha 1
+a=1
+b=2
+c=3
+print(a+b+c)
+print(a+b-c)
+print(a*b//c)
+print(a*b%c)
+print((a**b/2)-c*3)
+
+#zadacha 2
+a=3
+b=4
+s=0.5*(a*b)
+c=pow((pow(b,2)+(pow(a,2))),0.5)
+print(c)
+
+
+#zadacha str1
+string='hello world','a b c','torrent','five','guns',
+result=len(string)
+print(result)
+
+#zadacha str2
+string='tehno'
+print(string[3])
+print(string[3])
+print(string[0:5])
+print(string[0:3])
+print(string[::2])
+print(string[2::4])
+print(string[::-1])
+print(string[::-2])
+len(string)
+
+#zadacha na vichislenie1
+cel_chislo=(125)
+cel_chislo1=(str(cel_chislo)[-1])
+print(cel_chislo1)
+
+
+#zadacha na vichislenie2
+natur_chislo=(125)
+cel1=(str(natur_chislo)[-2])
+print(cel1)
